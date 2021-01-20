8cbec51d7c7c0ccb6c5450a6de2d8626e273a6f6
RayaneElimam <76107114+RayaneElimam@users.noreply.github.com>
2021-01-20 17:19:35 +0100

    Add files via upload

--- test.R
+++ test.R
@@ -9,35 +9,37 @@ clean_data <- oid[which(oid$location == "France"),]
 clean_data <- clean_data[,c(4,6,9)]
 clean_data <- clean_data[complete.cases(clean_data),]
 
-
-noised_data <- noise(clean_data,3,0.5)$noised_data
-
-imputed_by_mean <- mean_imputation(noised_data,clean_data,3)$imputed_data
-imputed_by_learning <- learning_imputation(noised_data,clean_data,1,3)$imputed_data 
-
-
 n_neighbor <- 10
 prediction_horizon <- 7
 start_day <- 60
 to_remove <- 1
 target <- 3
+target_name <- "new_deaths"
 reference <- clean_data
+noise_level <- 0.5
+
+noised_data <- noise(clean_data,target,noise_level)$noised_data
+
+imputed_by_mean <- mean_imputation(noised_data,clean_data,3)$imputed_data
+imputed_by_learning <- learning_imputation(noised_data,clean_data,to_remove,target)$imputed_data 
+
+
 
 
 #KNN
-knn_result_imputation_by_mean <- evaluation_unwknn(imputed_by_mean[,-to_remove],"new_deaths",reference[,-to_remove],start_day,prediction_horizon,n_neighbor)
+knn_result_imputation_by_mean <- evaluation_unwknn(imputed_by_mean[,-to_remove],target_name,reference[,-to_remove],start_day,prediction_horizon,n_neighbor)
 mean(knn_result_imputation_by_mean)
 
-knn_result_imputation_by_learning <- evaluation_unwknn(imputed_by_learning[,-to_remove],"new_deaths",reference[,-to_remove],start_day,prediction_horizon,n_neighbor)
+knn_result_imputation_by_learning <- evaluation_unwknn(imputed_by_learning[,-to_remove],target_name,reference[,-to_remove],start_day,prediction_horizon,n_neighbor)
 mean(knn_result_imputation_by_learning)
 
 
 #EKNN
 
-eknn_result_imputation_by_mean <- evaluation_eknn(imputed_by_mean[,-to_remove],"new_deaths",reference[,-to_remove],start_day,prediction_horizon,n_neighbor)
+eknn_result_imputation_by_mean <- evaluation_eknn(imputed_by_mean[,-to_remove],target_name,reference[,-to_remove],start_day,prediction_horizon,n_neighbor)
 mean(eknn_result_imputation_by_mean)
 
-eknn_result_imputation_by_learning <- evaluation_eknn(imputed_by_learning[,-to_remove],"new_deaths",reference[,-to_remove],start_day,prediction_horizon,n_neighbor)
+eknn_result_imputation_by_learning <- evaluation_eknn(imputed_by_learning[,-to_remove],target_name,reference[,-to_remove],start_day,prediction_horizon,n_neighbor)
 mean(eknn_result_imputation_by_learning)
 
 
